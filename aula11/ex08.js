330ae05fb31bd82286711d62142c7412369eb5c2
PedroAugustooDEV <augustobert@gmail.com>
2023-05-02 18:54:59 -0300

    Conditional Statement

---
+++ aula11/ex08.js
@@ -0,0 +1,9 @@
+
+//condicional simples
+var vel = 121
+
+console.log(`a velocidade do veículo é de ${vel}Km/h. `)
+if (vel > 120) {  
+    console.log(`Vai mais devagar ai o arrombado!!!`)
+}  //acho que um Else ficaria bom aqui, mas no curso o professor não colocou, por isso vou deixar sem!
+    console.log(`Dirija sempre utilizando o cinto de segurança!`)
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if ( país == 'Brasil') {
+    console.log(`Você é BRAZUKAAAAA!!`)
+} else {
+    console.log(`Sai da minha terra seu ladrão de ouro!!`)
+}
