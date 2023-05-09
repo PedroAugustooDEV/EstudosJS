08902f2fdc7fd2360a2802199d1dea4d3c8bfd55
PedroAugustooDEV <augustobert@gmail.com>
2023-05-08 22:22:22 -0300

    js

---
+++ aula12/ex12.js
@@ -0,0 +1,13 @@
+//var agora = new Date()
+var hora = 0  //agora.getHours()
+console.log (`agora são ${hora} horas`)
+
+if (hora < 6) {
+    console.log('Boa madrugada')
+} else if (hora < 12 ) {
+    console.log('Bom dia!!')
+} else if (hora < 18) {
+    console.log ('Boa tarde!')
+} else if (hora < 24) {
+    console.log('boa noite')
+} 
